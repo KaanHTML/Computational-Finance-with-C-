240593df1d091d9555a2bec2047b601403502211
Kaan Abudak <59143443+KaanHTML@users.noreply.github.com>
2024-01-15 17:49:55 +0100

    Add files via upload

---
+++ Numerical Methods in Finance with C++/5. Monte Carlo Simulation/5.1 AsianOption/BSModel01.h
@@ -0,0 +1,17 @@
+#ifndef BSModel_H
+#define BSModel_H
+using namespace std;
+#include<vector>
+
+typedef vector <double> SamplePath;
+class BSModel
+{
+	public:	
+		double S0, r, sigma;
+		BSModel(double S0_, double r_, double sigma_)
+		{
+			S0 = S0_; r=r_; sigma = sigma_; srand(time(NULL));
+		}
+		void GenerateSamplePath(double T, int m, SamplePath& S);
+};
+#endif
---
+++ Numerical Methods in Finance with C++/5. Monte Carlo Simulation/5.1 AsianOption/PathDepOption.h
@@ -0,0 +1,24 @@
+#ifndef PathDep_h
+#define PathDep_h
+
+#include "BSModel01.h"
+
+class PathDepOption
+{
+	public:
+		double T;
+		int m;
+		double PriceByMC(BSModel Model, long N);
+		virtual double Payoff(SamplePath& S) = 0;
+};
+
+class ArthmAsianCall: public PathDepOption
+{
+	public:
+		double K;
+		ArthmAsianCall(double T_, double K_, int m_)
+			{T=T_; K=K_; m=m_;}
+		double Payoff(SamplePath& S); 
+};
+
+#endif	
